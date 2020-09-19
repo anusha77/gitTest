e0dd23d3974ba126edd284e33397b4ef5e112f77
Anusha <anusha121293@gmail.com>
2020-09-19 05:43:45 +0530

    sample commit

--- sketch.js
+++ sketch.js
@@ -44,7 +44,7 @@ function draw(){
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
     box1.display();
-    box2.display();
+    //box2.display();
     ground.display();
     pig1.display();
     log1.display();
